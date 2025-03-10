7ff47dfab1dc40f00a7119871375fa7403cfc8a4
zoujin <zoujin@zj.com>
2025-03-10 17:39:17 +0800

    更新

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<Person/>
+</template>
+
+<script lang="ts">
+import Person from './components/Person.vue';
+
+export default{
+    name:'App',//组件名
+    components:{Person}
+}
+</script>
+<style>
+</style>
